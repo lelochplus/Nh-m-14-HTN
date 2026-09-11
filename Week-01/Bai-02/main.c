8dfd6c2242b69d65948477d4bf7ea3057e571edc
lelochplus <164453161+lelochplus@users.noreply.github.com>
2026-09-11 12:53:31 +0700

    Update main.c

--- Week-01/Bai-02/main.c
+++ Week-01/Bai-02/main.c
@@ -1,6 +1,5 @@
 #include <stdint.h>
 
-/* ================= RCC ================= */
 
 #define RCC_BASE        0x40021000UL
 #define RCC_APB2ENR     (*(volatile uint32_t *)(RCC_BASE + 0x18))
@@ -8,7 +7,6 @@
 #define IOPAEN          (1U << 2)
 
 
-/* ================= GPIOA ================= */
 
 #define GPIOA_BASE      0x40010800UL
 
@@ -16,7 +14,6 @@
 #define GPIOA_ODR       (*(volatile uint32_t *)(GPIOA_BASE + 0x0C))
 
 
-/* ================= Delay ================= */
 
 void delay_ms(uint32_t ms)
 {
@@ -32,31 +29,19 @@ void delay_ms(uint32_t ms)
 }
 
 
-/* ================= GPIO Init ================= */
 
 void gpio_init(void)
 {
-    /* Enable clock for GPIOA */
     RCC_APB2ENR |= IOPAEN;
 
-    /*
-     * PA0 -> PA7
-     *
-     * Mỗi chân:
-     * MODE = 01 -> Output 10 MHz
-     * CNF  = 00 -> General purpose push-pull
-     *
-     * Giá trị cấu hình = 0x1
-     */
+
 
     GPIOA_CRL = 0x11111111;
 
-    /* Tắt tất cả LED ban đầu */
     GPIOA_ODR &= ~0xFFU;
 }
 
 
-/* ================= Main ================= */
 
 int main(void)
 {
@@ -64,7 +49,6 @@ int main(void)
 
     while (1)
     {
-        /* ================= PA0 -> PA7 ================= */
 
         for (int i = 0; i < 8; i++)
         {
@@ -74,7 +58,6 @@ int main(void)
         }
 
 
-        /* ================= PA7 -> PA0 ================= */
 
         for (int i = 6; i > 0; i--)
         {
@@ -83,9 +66,6 @@ int main(void)
             delay_ms(100);
         }
 
-        /*
-         * Sau khi PA1 sáng,
-         * vòng lặp quay lại PA0.
-         */
+      
     }
 }
